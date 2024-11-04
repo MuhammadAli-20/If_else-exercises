a56139d2bafbeb39361eb193b9c3c81d45260545
Muhammad Ali <alioraf789@gmail.com>
2024-11-04 10:54:55 +0400

    If else

---
+++ 3-Multicon.py
@@ -0,0 +1,6 @@
+num = int(input("Enter a number: "))
+
+if num > 0 and num < 10:
+    print("The number is a single-digit positive number.")
+else:
+    print("The number is either negative or has more than one digit.")
